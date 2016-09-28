b28926f462fad2b92e8f247fa838d63d06bc9e7c
lichengtao <372425159@qq.com>
2016-09-28 15:52:32 +0800

    revise

--- downloadPC/js/operate.js
+++ downloadPC/js/operate.js
@@ -1,26 +1,24 @@
 $(function(){
 
-if($('#poimg_lct01')[0].getBoundingClientRect().top>700){
-	$('#poimg_lct01').addClass('addScale');
-};
-if($('#poimg_lct02')[0].getBoundingClientRect().top>700){
-	$('#poimg_lct02').addClass('translateXY');
-};
+// if($('#poimg_lct01')[0].getBoundingClientRect().top>700){
+// 	$('#poimg_lct01').addClass('addScale');
+// };
+// if($('#poimg_lct02')[0].getBoundingClientRect().top>700){
+// 	$('#poimg_lct02').addClass('translateXY');
+// };
 
 $(window).scroll(function(event) {
 	/* Act on the event */
-	var animate1=$('#poimg_lct01')[0];
-	var animate2=$('#poimg_lct02')[0];
+var animate1=$('#poimg_lct01')[0];	
 var bounding=animate1.getBoundingClientRect();
-var bounding2=animate2.getBoundingClientRect();
 var top1=bounding.top;
+var animate2=$('#poimg_lct02')[0];
+var bounding2=animate2.getBoundingClientRect();
 var top2=bounding2.top;
-
-console.log(top2)
-if(top1>700){
+if(top1<700){
 	$('#poimg_lct01').addClass('addScale');
 };
-if(top2>500){
+if(top2<1000){
 	$('#poimg_lct02').addClass('translateXY');
 };
 
